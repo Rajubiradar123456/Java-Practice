7d2a8a65383ee23c509e23493236eb723bc96800
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-09-01 22:13:05 +0530

    finding the largest of three numbers

---
+++ largestof3number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class largestof3number {
+    public static void main(String[] args) {
+         int a=4 ,b=6, c=8;
+         if(a>b && a>c){
+            System.out.println("a is greaer");
+         }
+         else if(b>c && b>a){
+            System.out.println("b is gr");
+         }
+         else{
+            System.out.println("c is g");
+         }
+    }
+}
